f2ef0e471aa19335c2ccc9def0420eba76c043d7
Hamish McKenzie-Campbell <geitaguy@gmail.com>
2018-06-16 08:39:23 +0800

    add group to sudo user

--- docker-install.sh
+++ docker-install.sh
@@ -5,7 +5,7 @@ docker-install() {
     echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
     apt-get -q update
     apt-get -qy install docker-ce
-    usermod -aG docker $(whoami)
+    usermod -aG docker "$SUDO_USER"
     curl -fsSLO /etc/bash_completion.d/docker https://raw.githubusercontent.com/docker/cli/b75596e1e4d5295ac69b9934d1bd8aff691a0de8/contrib/completion/bash/docker
     curl -fsSLO /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m)
     chmod +x /usr/local/bin/docker-compose
