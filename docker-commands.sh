df3a29a380ad317201eeccbad9ee842abad2d73f
Hamish McKenzie-Campbell <geitaguy@gmail.com>
2018-06-16 08:30:55 +0800

    make docker install sudo friendly

--- docker-commands.sh
+++ docker-commands.sh
@@ -7,21 +7,9 @@ runtimedir="${dcroot}/runtime"
 
 dsinstall() {
   if [ ! $UID = "0" ]; then
-    echo "$0 must be run as root"
-    echo "Usage: sudo $0"
-    exit 1
+    sudo ${dcroot}/docker-install.sh
   else
-    apt-get -q update
-    apt-get -qy install --no-install-recommends apt-transport-https ca-certificates curl gnupg2 software-properties-common
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-    echo '"deb [arch=amd64] https://download.docker.com/linux/debian    $(lsb_release -cs) stable"' > /etc/apt/sources.list.d/docker.list
-    apt-get -q update
-    apt-get -qy install docker-ce
-    usermod -aG docker $(whoami)
-    curl -O /etc/bash_completion.d/docker https://raw.githubusercontent.com/docker/cli/b75596e1e4d5295ac69b9934d1bd8aff691a0de8/contrib/completion/bash/docker
-    curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-    docker run hello-world
+    ${dcroot}/docker-install.sh
   fi
 }
 
---
+++ docker-install.sh
@@ -0,0 +1,21 @@
+docker-install() {
+    apt-get -q update
+    apt-get -qy install --no-install-recommends apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+    apt-get -q update
+    apt-get -qy install docker-ce
+    usermod -aG docker $(whoami)
+    curl -fsSLO /etc/bash_completion.d/docker https://raw.githubusercontent.com/docker/cli/b75596e1e4d5295ac69b9934d1bd8aff691a0de8/contrib/completion/bash/docker
+    curl -fsSLO /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m)
+    chmod +x /usr/local/bin/docker-compose
+    docker run hello-world
+}
+
+  if [ ! $UID = "0" ]; then
+    echo "$0 must be run as root"
+    echo "Usage: sudo $0"
+    exit 1
+  else
+    docker-install
+  fi
