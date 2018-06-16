31f757f0ce3ba131779c26fcb797271736ed0ee9
Hamish McKenzie-Campbell <geitaguy@gmail.com>
2018-06-16 14:15:29 +0800

    pass on docker-compose for now

--- docker-install.sh
+++ docker-install.sh
@@ -7,8 +7,8 @@ docker_install() {
     apt-get -qy install docker-ce
     usermod -aG docker "$SUDO_USER"
     curl -fsSLO /etc/bash_completion.d/docker https://raw.githubusercontent.com/docker/cli/b75596e1e4d5295ac69b9934d1bd8aff691a0de8/contrib/completion/bash/docker
-    curl -fsSLO /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m)
-    chmod +x /usr/local/bin/docker-compose
+    #curl -fsSLO /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m)
+    #chmod +x /usr/local/bin/docker-compose
     docker run hello-world
 }
 
